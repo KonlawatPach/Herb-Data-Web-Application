f88fa5098341eb79005cd687d68575f3f8553922
KonlawatPach <pach.wongsrithong@Gmail.com>
2023-01-13 22:28:50 +0700

    red error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Route } from '@angular/router';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { SearchpageComponent } from './searchpage/searchpage.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HerbBoxComponent } from './searchpage/herb-box/herb-box.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchpageComponent,
     NavbarComponent,
-    HerbBoxComponent
+    HerbBoxComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
